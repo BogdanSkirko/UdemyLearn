95ea73913a13836bdf16e102420015d48be6df41
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-07-23 16:24:41 +0300

    FirstHomeworkFromUdemy

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-let numberOfFilms = prompt('Скільки фільмів ви переглянули?','')
-console.log(numberOfFilms);
+const numberOfFilms = +prompt('Скільки фільмів ви переглянули?','')
 
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+}
+const firstQuestion = prompt('Один из последних просмотренных фильмов?','')
+const secondQuestion = +prompt('На сколько оцените его?','')
+personalMovieDB.movies[firstQuestion] = secondQuestion
