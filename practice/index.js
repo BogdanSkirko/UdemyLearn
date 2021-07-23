c8b90ec0844be4ab997867aa1efa67618f3999b2
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-07-23 17:07:28 +0300

    2#HomeworkFromUdemy

--- practice/index.js
+++ practice/index.js
@@ -7,6 +7,19 @@ let personalMovieDB = {
     genres: {},
     privat: false
 }
-const firstQuestion = prompt('Один из последних просмотренных фильмов?','')
-const secondQuestion = +prompt('На сколько оцените его?','')
-personalMovieDB.movies[firstQuestion] = secondQuestion
+// const firstQuestion = prompt('Один из последних просмотренных фильмов?','')
+// const secondQuestion = +prompt('На сколько оцените его?','')
+// personalMovieDB.movies[firstQuestion] = secondQuestion
+
+for (let i=0; i<2; i++) {
+    const firstQuestion = prompt('Один из последних просмотренных фильмов?',''),
+          secondQuestion = +prompt('На сколько оцените его?','')
+    if (firstQuestion != null && secondQuestion != null && firstQuestion != '' && secondQuestion != '' && firstQuestion.length < 50){
+        personalMovieDB.movies[firstQuestion] = secondQuestion;
+        console.log('Ok');
+    }
+    else {
+        console.log('No,Введіть менше 50 символів');
+        i--
+    }
+}
